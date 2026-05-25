8f9c3ea38d4aa4041e6a4023577d95fd9127d223
EduardoMoraess <eduzinho2118@gmail.com>
2026-05-25 10:37:03 -0300

    rotas feitas com sucesso

--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,21 @@
 }
 acessoProjetos()*/
 
+const acesso = document.getElementById('acess');
+const modal = document.querySelector('dialog');
 
+acesso.addEventListener('click', ()=>{
+    modal.showModal();
+})
+
+function configurarRedirecionamento(idElemento, url) {
+    const elemento = document.getElementById(idElemento);
+    if (elemento) {
+        elemento.addEventListener('click', () => {
+            location.href = url;
+        });
+    }
+}
+// Uso:
+configurarRedirecionamento('multi-tools', 'src/pages/multi-projetos.html');
+configurarRedirecionamento('suporte', 'src/pages/help_disk.html');
